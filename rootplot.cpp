7116ad6c692b5f137c0a993e818725ee87bfcddc
gaspardL2 <gaspard.tourk@tutanota.com>
2022-09-23 18:01:50 +0200

    Initial commit- scientific cpp training for max-planck interview

---
+++ rootplot.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hawking on 23/09/22.
+//
+
+#include "rootplot.h"
---
+++ rootplot.h
@@ -0,0 +1,14 @@
+//
+// Created by hawking on 23/09/22.
+//
+
+#ifndef SCIENTIFIC_ROOTPLOT_H
+#define SCIENTIFIC_ROOTPLOT_H
+
+
+class rootplot {
+
+};
+
+
+#endif //SCIENTIFIC_ROOTPLOT_H
