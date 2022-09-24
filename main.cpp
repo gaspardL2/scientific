efda59386367d00800904949d2c0292a89df316d
gaspardL2 <gaspard.tourk@tutanota.com>
2022-09-24 17:33:40 +0200

    Initial commit- scientific cpp training for max-planck interview
    Added classes "rootplot" and "astro"

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <cmath>
+#include "TApplication.h"
+#include "TF1.h"
+#include "TCanvas.h"
+#include "TRootCanvas.h"
+#include "TMath.h"
 
-int main() {
+int main(int argc, char **argv) {
+    TApplication app("app", &argc, argv);
+    TCanvas* c = new TCanvas("c", "Some Test", 0, 0, 800, 600);
+    TF1 *f1 = new TF1("f1", "sin(x)", -25, 25);
 
+    f1->SetLineColor(kBlue+1);
+    f1->SetLineWidth(6);
+    f1->SetTitle("My graph;x;x sin(x)");
+    f1->Draw();
+    c->Modified(); c->Update();
+
+    TRootCanvas *rc = (TRootCanvas *)c->GetCanvasImp();
+    rc->Connect("CloseWindow()", "TApplication", gApplication, "Terminate()");
+
+    app.Run();
     return 0;
 }
